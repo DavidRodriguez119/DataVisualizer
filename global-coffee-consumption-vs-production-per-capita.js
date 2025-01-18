cf735feccf27fc4a6f1cfb8b3f1eabb279c3b265
David Rodriguez <santiago1.dsrr@gmail.com>
2025-01-18 11:22:26 -0500

    copy content into directory

---
+++ global-coffee-consumption-vs-production-per-capita.js
@@ -0,0 +1,7 @@
+function GlobalCoffeeConsumptionVsProduction() {
+  // Name for the visualization to appear in the menu.
+  this.name = 'Global Coffee Consumption vs Production Per Capita';
+
+  // Unique ID for the visualization.
+  this.id = 'global-coffee-consumption-vs-production-per-capita';
+}
